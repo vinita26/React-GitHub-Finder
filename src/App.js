64a8ab0ce5d8864136f2ab7bb839253612d081d3
Vinita Gupta <vinita.gupta@publicissapient.com>
2020-02-17 09:20:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
+import User from './components/user/Users';
+import axios from 'axios';
+import Search from './components/user/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  searchUsers = (text) => {
+    console.log('text ', text);
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Navbar title="Github Finder" />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} />
+          <User loading={this.state.loading} users={this.state.users} />
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = (props) => {
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+            <i className="fab"></i>
+                GitHub Finder
+            </h1>
+        </nav>
+    )
+}
+
+export default Navbar
